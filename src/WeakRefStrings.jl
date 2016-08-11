34dc86ff7622a9e1959e0d33ad050ae6d04a28b5
Jacob Quinn <jacob.quinn@domo.com>
2016-08-11 01:02:48 -0600

    Updates

--- src/WeakRefStrings.jl
+++ src/WeakRefStrings.jl
@@ -14,20 +14,23 @@ once the parent object goes out of scope (i.e. loses a reference to it)**
 """
 immutable WeakRefString{T} <: AbstractString
     ptr::Ptr{T}
-    len::Int
+    len::Int # of **code units**
+    ind::Int
 end
+
+WeakRefString{T}(ptr::Ptr{T}, len) = WeakRefString(ptr, Int(len), 0)
 
 const NULLSTRING = WeakRefString(Ptr{UInt8}(0), 0)
 const NULLSTRING16 = WeakRefString(Ptr{UInt16}(0), 0)
 const NULLSTRING32 = WeakRefString(Ptr{UInt32}(0), 0)
 Base.endof(x::WeakRefString) = x.len
 Base.length(x::WeakRefString) = x.len
-Base.next(x::WeakRefString, i::Int) = (Char(unsafe_load(x.ptr, i)),i+1)
+Base.next(x::WeakRefString, i::Int) = (Char(unsafe_load(x.ptr, i)), i + 1)
 
 import Base: ==
 function ==(x::WeakRefString{UInt8}, y::WeakRefString{UInt8})
-    x.len==y.len && (x.ptr==y.ptr || ccall(:memcmp, Cint, (Ptr{UInt8}, Ptr{UInt8}, Csize_t),
-                                           x.ptr, y.ptr, x.len)==0)
+    x.len == y.len && (x.ptr == y.ptr || ccall(:memcmp, Cint, (Ptr{UInt8}, Ptr{UInt8}, Csize_t),
+                                           x.ptr, y.ptr, x.len) == 0)
 end
 
 function Base.hash(s::WeakRefString{UInt8}, h::UInt)
@@ -36,8 +39,14 @@ function Base.hash(s::WeakRefString{UInt8}, h::UInt)
 end
 
 Base.show{T}(io::IO, ::Type{WeakRefString{T}}) = print(io, "WeakRefString{$T}")
-Base.show(io::IO, x::WeakRefString{UInt16}) = print(io, x == NULLSTRING16 ? "\"\"" : "\"$(utf16(x.ptr, x.len))\"")
-Base.show(io::IO, x::WeakRefString{UInt32}) = print(io, x == NULLSTRING32 ? "\"\"" : "\"$(utf32(x.ptr, x.len))\"")
+function Base.show{T}(io::IO, x::WeakRefString{T})
+    print('"')
+    for c in x
+        print(c)
+    end
+    print('"')
+    return
+end
 Base.string(x::WeakRefString{UInt16}) = x == NULLSTRING16 ? utf16("") : utf16(x.ptr, x.len)
 Base.string(x::WeakRefString{UInt32}) = x == NULLSTRING32 ? utf32("") : utf32(x.ptr, x.len)
 Base.convert(::Type{WeakRefString{UInt16}}, x::UTF16String) = WeakRefString(pointer(x.data), length(x))
@@ -47,16 +56,13 @@ if !isdefined(Core, :String)
     Base.convert(::Type{ASCIIString}, x::WeakRefString) = convert(ASCIIString, string(x))
     Base.convert(::Type{UTF8String}, x::WeakRefString) = convert(UTF8String, string(x))
     Base.string(x::WeakRefString) = x == NULLSTRING ? utf8("") : utf8(x.ptr, x.len)
-	Base.show(io::IO, x::WeakRefString) = print(io, x == NULLSTRING ? "\"\"" : "\"$(utf8(x.ptr, x.len))\"")
     Base.convert(::Type{WeakRefString{UInt8}}, x::UTF8String) = WeakRefString(pointer(x.data), length(x))
     Base.convert(::Type{WeakRefString{UInt8}}, x::ASCIIString) = WeakRefString(pointer(x.data), length(x))
 else
     Base.convert(::Type{WeakRefString{UInt8}}, x::String) = WeakRefString(pointer(x.data), length(x))
     Base.convert(::Type{String}, x::WeakRefString) = convert(String, string(x))
 	Base.string(x::WeakRefString) = x == NULLSTRING ? "" : unsafe_string(x.ptr, x.len)
-	Base.show(io::IO, x::WeakRefString) = print(io, x == NULLSTRING ? "\"\"" : "\"$(unsafe_wrap(String, x.ptr, x.len))\"")
     Base.String(x::WeakRefString) = string(x)
 end
-
 
 end # module
