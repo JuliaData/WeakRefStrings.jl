1442f9acfc112f2498a3268ef56781332cb29ce1
Alexey Stukalov <astukalov@gmail.com>
2017-09-20 14:06:09 +0000

    add testsets

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,57 +1,69 @@
 using WeakRefStrings, Base.Test
 
-data = Vector{UInt8}("hey there sailor")
+@testset "WeakRefString{UInt8}" begin
+    data = Vector{UInt8}("hey there sailor")
 
-str = WeakRefString(pointer(data), 3)
+    str = WeakRefString(pointer(data), 3)
 
-@test length(str) == 3
-for (i,c) in enumerate(str)
-    @test data[i] == c % UInt8
-end
-@test string(str) == "hey"
-@test String(str) == "hey"
+    @test length(str) == 3
+    for (i,c) in enumerate(str)
+        @test data[i] == c % UInt8
+    end
+    @test string(str) == "hey"
+    @test String(str) == "hey"
 
-io = IOBuffer()
-show(io, str)
-@test String(take!(io)) == "\"hey\""
+    io = IOBuffer()
+    show(io, str)
+    @test String(take!(io)) == "\"hey\""
 
-show(io, typeof(str))
-@test String(take!(io)) == "WeakRefString{UInt8}"
-
-A = WeakRefStringArray(data, [str])
-
-# simulate UTF16 data
-data = [0x0068, 0x0065, 0x0079]
-
-str = WeakRefString(pointer(data), 3)
-@test typeof(str) == WeakRefString{UInt16}
-@test String(str) == "hey"
-@test length(str) == 3
-for (i,c) in enumerate(str)
-    @test data[i] == c % UInt8
+    show(io, typeof(str))
+    @test String(take!(io)) == "WeakRefString{UInt8}"
 end
 
-# simulate UTF32 data with trailing null byte
-data = [0x00000068, 0x00000065, 0x00000079, 0x00000000]
+@testset "WeakRefString{UInt16}" begin
+    # simulate UTF16 data
+    data = [0x0068, 0x0065, 0x0079]
 
-str = WeakRefString(pointer(data), 4)
-@test typeof(str) == WeakRefString{UInt32}
-@test String(str) == "hey"
-@test length(str) == 4
-for (i,c) in enumerate(str)
-    @test data[i] == c % UInt8
+    str = WeakRefString(pointer(data), 3)
+    @test typeof(str) == WeakRefString{UInt16}
+    @test String(str) == "hey"
+    @test length(str) == 3
+    for (i,c) in enumerate(str)
+        @test data[i] == c % UInt8
+    end
 end
 
-A = WeakRefStringArray(UInt8[], WeakRefString{UInt8}, 10)
-@test size(A) == (10,)
-@test A[1] == ""
-@test A[1, 1] == ""
-A[1] = "hey"
-A[1, 1] = "hey"
-@test A[1] == "hey"
-resize!(A, 5)
-@test size(A) == (5,)
-B = WeakRefStringArray(UInt8[], WeakRefString{UInt8}, 10)
-B[1] = "ho"
-append!(A, B)
-@test size(A) == (15,)
+@testset "WeakRefString{UInt32}" begin
+    # simulate UTF32 data with trailing null byte
+    data = [0x00000068, 0x00000065, 0x00000079, 0x00000000]
+
+    str = WeakRefString(pointer(data), 4)
+    @test typeof(str) == WeakRefString{UInt32}
+    @test String(str) == "hey"
+    @test length(str) == 4
+    for (i,c) in enumerate(str)
+        @test data[i] == c % UInt8
+    end
+end
+
+@testset "WeakRefArray" begin
+    data = Vector{UInt8}("hey there sailor")
+    str = WeakRefString(pointer(data), 3)
+    C = WeakRefStringArray(data, [str])
+    @test length(C) == 1
+
+    A = WeakRefStringArray(UInt8[], WeakRefString{UInt8}, 10)
+    @test size(A) == (10,)
+    @test A[1] == ""
+    @test A[1, 1] == ""
+    A[1] = "hey"
+    A[1, 1] = "hey"
+    @test A[1] == "hey"
+    resize!(A, 5)
+    @test size(A) == (5,)
+
+    B = WeakRefStringArray(UInt8[], WeakRefString{UInt8}, 10)
+    B[1] = "ho"
+    append!(A, B)
+    @test size(A) == (15,)
+end
