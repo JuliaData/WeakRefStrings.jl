cdb509aa0ebf3f5eb38c57fdc14cc7a1f48f9b85
Jacob Quinn <quinn.jacobd@gmail.com>
2021-04-30 02:22:56 -0600

    WIP: Introduce new PosLenString/PosLenStringVector types
    The aim of these new types is to generalize an idea started in CSV.jl
    where we wish to 1) avoid allocating full Strings while parsing and 2)
    keep the "string" data as lazy as possible post-parsing. We move the
    code for `PosLen` directly from CSV.jl, which is basically a `UInt64`
    that stores 20 bits for a strings length, 42 bits for the byte position
    where a string begins in a data `Vector{UInt8}` buffer, 1 bit to flag if
    a string value is actually `missing`, and the last 1 bit to flag if the
    string value has escape characters and needs to be unescaped.
    
    The PosLenStringVector type takes a data buffer, be it a mmapped file or
    otherwise, and a `Vector{PosLen}`, along with the escape character (as a
    UInt8). When indexed, this custom array type will return a
    `PosLenString`, which keeps a reference to the original data buffer (yet
    may still be stack allocated due to new compiler optimizations), along
    with the string's specific `PosLen` to know where its data lives. Note
    that once a `PosLenString` has been materialized, we no longer care
    about the `missing` bit, since that must be handled in the
    `PosLenStringVector` getindex method.
    
    The `PosLenString` type is an `AbstractString`, and I think it has
    really good coverage and performance compared to `Base.String`. I
    manually checked performance of each method tested in the
    `test/poslenstrings.jl` file. The two cases where there's a slight
    performance hit (30-50% slowdown) are `lpad`/`rpad` and `replace`, where
    Base algorithms currently force conversion to `String`, which incurs an
    extra copy for `PosLenString`. I'll investigate whether we can just copy
    the algorithms for those.
    
    Note that mush of the `PosLenString` code was copied from Base routines
    due to the current code organization which has optimizations for
    `String`, but slower fallbacks for `AbstractString`. `PosLenString`
    operates very similarly to `String`, so all those optimizations make
    sense for it as well, hence the large amount of copy/paste.
    
    Marking this PR as WIP because I've hardly spent any time on
    `PosLenStringVector` yet and need to spend the proper time ensuring it
    works properly and considering how we want to support mutability (either
    requiring user to do explicit `copy` or some kind of copy-on-write
    automatically on first mutating operation). The expectation is that when
    users request `lazystrings=true` from CSV.read, they'll be returned a
    `PosLenStringVector`, and may need to mutate values afterwards, and I'd
    like to support that if at all possible.

---
+++ src/poslenstrings.jl
@@ -0,0 +1,470 @@
+primitive type PosLen 64 end
+PosLen(x::UInt64) = Core.bitcast(PosLen, x)
+UInt64(x::PosLen) = Core.bitcast(UInt64, x)
+
+Base.convert(::Type{PosLen}, x::UInt64) = PosLen(x)
+Base.convert(::Type{UInt64}, x::PosLen) = UInt64(x)
+
+const MISSING_BIT = 0x8000000000000000
+missingvalue(x) = (UInt64(x) & MISSING_BIT) == MISSING_BIT
+
+const ESCAPE_BIT = 0x4000000000000000
+escapedvalue(x) = (UInt64(x) & ESCAPE_BIT) == ESCAPE_BIT
+
+getpos(x) = (UInt64(x) & 0x3ffffffffff00000) >> 20
+getlen(x) = UInt64(x) & 0x00000000000fffff
+
+@noinline lentoolong(len) = throw(ArgumentError("len = $len too long"))
+
+@inline function PosLen(pos, len, missing=false, escaped=false)
+    len > 1048575 && lentoolong(len)
+    pos = UInt64(pos) << 20
+    pos |= ifelse(missing, MISSING_BIT, UInt64(0))
+    pos |= ifelse(escaped, ESCAPE_BIT, UInt64(0))
+    return PosLen(pos | UInt64(len))
+end
+
+# custom string type
+struct PosLenString <: AbstractString
+    data::Vector{UInt8}
+    poslen::PosLen
+    e::UInt8
+    inds::Vector{Int} # only for escaped strings
+    
+    function PosLenString(d::Vector{UInt8}, p::PosLen, e::UInt8)
+        if escapedvalue(p)
+            maxpos = Int(getpos(p) + getlen(p) - 1)
+            inds = [Int(x) for x = getpos(p):maxpos if !((x == getpos(p) && d[x] == e) || (x > getpos(p) && d[x - 1] != e && d[x] == e))]
+            return new(d, p, e, inds)
+        else
+            return new(d, p, e)
+        end
+    end
+end
+
+# convenience constructor for testing
+function str(x::Union{Missing, String}, e=nothing)
+    data = Vector{UInt8}(x)
+    poslen = PosLen(1, sizeof(coalesce(x, "")), x === missing, e !== nothing && UInt8(e) in data)
+    return PosLenString(data, poslen, UInt8(something(e, 0x00)))
+end
+
+pos(x::PosLenString) = Int(getpos(x.poslen))
+len(x::PosLenString) = Int(getlen(x.poslen))
+escaped(x::PosLenString) = escapedvalue(x.poslen)
+
+Base.codeunits(x::PosLenString) =
+    escaped(x) ? view(x.data, x.inds) : view(x.data, pos(x):(pos(x) + len(x) - 1))
+
+Base.ncodeunits(x::PosLenString) = !escaped(x) ? len(x) : length(x.inds)
+Base.codeunit(x::PosLenString) = UInt8
+Base.@propagate_inbounds function Base.codeunit(x::PosLenString, i::Int)
+    @boundscheck checkbounds(Bool, x, i) || throw(BoundsError(x, i))
+    poslen = x.poslen
+    return @inbounds escapedvalue(poslen) ? x.data[x.inds[i]] : x.data[getpos(poslen) + i - 1]
+end
+
+Base.pointer(x::PosLenString, i::Integer=1) = pointer(x.data, pos(x) + i - 1)
+
+# Base.string(x::PosLenString) = x
+Base.String(x::PosLenString) =
+    !escaped(x) ? unsafe_string(pointer(x), len(x)) : String(codeunits(x))
+Base.Vector{UInt8}(x::PosLenString) =
+    !escaped(x) ? x.data[pos(x):(pos(x) + len(x) - 1)] : copy(codeunits(x))
+Base.Array{UInt8}(x::PosLenString) = Vector{UInt8}(x)
+Base.Symbol(x::PosLenString) =
+    escaped(x) ? Symbol(String(x)) : ccall(:jl_symbol_n, Ref{Symbol}, (Ptr{UInt8}, Int), pointer(x), len(x))
+
+function ==(x::PosLenString, y::PosLenString)
+    l = ncodeunits(x)
+    l == ncodeunits(y) || return false
+    if !escaped(x) && !escaped(y)
+        return ccall(:memcmp, Cint, (Ptr{UInt8}, Ptr{UInt8}, Csize_t),
+                     pointer(x), pointer(y), l) == 0
+    end
+    # escaped string path
+    for i = 1:l
+        codeunit(x, i) == codeunit(y, i) || return false
+    end
+    return true
+end
+
+function ==(x::String, y::PosLenString)
+    sizeof(x) == sizeof(y) || return false
+    if !escaped(y)
+        ccall(:memcmp, Cint, (Ptr{UInt8}, Ptr{UInt8}, Csize_t),
+              pointer(x), pointer(y), sizeof(x)) == 0
+    end
+    # escaped string path
+    for i = 1:sizeof(y)
+        codeunit(x, i) == codeunit(y, i) || return false
+    end
+    return true
+end
+==(y::PosLenString, x::String) = x == y
+
+function Base.cmp(a::PosLenString, b::PosLenString)
+    al, bl = len(a), len(b)
+    if !escaped(a) && !escaped(b)
+        c = ccall(:memcmp, Int32, (Ptr{UInt8}, Ptr{UInt8}, Csize_t),
+                  pointer(a), pointer(b), min(al, bl))
+        return c < 0 ? -1 : c > 0 ? +1 : cmp(al, bl)
+    end
+    # TODO: could be faster by looping directly through codeunits
+    return cmp(codeunits(a), codeunits(b))
+end
+
+function Base.hash(s::PosLenString, h::UInt)
+    h += Base.memhash_seed
+    if !escaped(s)
+        ccall(Base.memhash, UInt, (Ptr{UInt8}, Csize_t, UInt32), pointer(s), len(s), h % UInt32) + h
+    else
+        # TODO: this is really expensive, even for rare escaped PosLenString
+        # this makes it about 4x slower than hash(::String)
+        # alternative is to maybe take what's needed from MurmurHash3.jl to operate by codeunit
+        x = copy(codeunits(s))
+        ccall(Base.memhash, UInt, (Ptr{UInt8}, Csize_t, UInt32), pointer(x), sizeof(x), h % UInt32) + h
+    end
+end
+
+function Base.write(io::IO, x::PosLenString)
+    if !escaped(x)
+        return GC.@preserve x Int(unsafe_write(io, pointer(x), reinterpret(UInt, sizeof(x))))::Int
+    end
+    len = ncodeunits(x)
+    for i = 1:len
+        @inbounds write(io, codeunit(x, i))
+    end
+    return len
+end
+
+# optimize IOBuffer case; modified copy/paste from iobuffer.jl
+function Base.write(io::IOBuffer, x::PosLenString)
+    if !escaped(x)
+        return GC.@preserve x Int(unsafe_write(io, pointer(x), reinterpret(UInt, sizeof(x))))::Int
+    end
+    nb = ncodeunits(x)
+    Base.ensureroom(io, nb)
+    ptr = (io.append ? io.size+1 : io.ptr)
+    written = Int(min(nb, Int(length(io.data))::Int - ptr + 1))
+    iowrite = written
+    d = io.data
+    p = 1
+    while iowrite > 0
+        @inbounds d[ptr] = codeunit(x, p)
+        ptr += 1
+        p += 1
+        iowrite -= 1
+    end
+    io.size = max(io.size, ptr - 1)
+    if !io.append
+        io.ptr += written
+    end
+    return written
+end
+Base.print(io::IO, s::PosLenString) = (write(io, s); nothing)
+
+# copy/pasted from substring.jl
+function Base.reverse(s::PosLenString)
+    # Read characters forwards from `s` and write backwards to `out`
+    out = Base._string_n(sizeof(s))
+    offs = sizeof(s) + 1
+    for c in s
+        offs -= ncodeunits(c)
+        Base.__unsafe_string!(out, c, offs)
+    end
+    return out
+end
+
+@inline function Base.__unsafe_string!(out, s::PosLenString, offs::Integer)
+    n = sizeof(s)
+    if !escaped(s)
+        GC.@preserve s out unsafe_copyto!(pointer(out, offs), pointer(s), n)
+    else
+        for i = 1:n
+            @inbounds unsafe_store!(pointer(out, offs + i - 1), codeunit(s, i))
+        end
+    end
+    return n
+end
+
+function Base.string(a::PosLenString...)
+    n = 0
+    for v in a
+        n += sizeof(v)
+    end
+    out = Base._string_n(n)
+    offs = 1
+    for v in a
+        offs += Base.__unsafe_string!(out, v, offs)
+    end
+    return out
+end
+
+function Base.repeat(s::PosLenString, r::Integer)
+    r < 0 && throw(ArgumentError("can't repeat a string $r times"))
+    r == 0 && return ""
+    r == 1 && return s
+    n = sizeof(s)
+    out = Base._string_n(n * r)
+    if n == 1 # common case: repeating a single-byte string
+        @inbounds b = codeunit(s, 1)
+        ccall(:memset, Ptr{Cvoid}, (Ptr{UInt8}, Cint, Csize_t), out, b, r)
+    else
+        for i = 0:r-1
+            if !escaped(s)
+                GC.@preserve s out unsafe_copyto!(pointer(out, i * n + 1), pointer(s), n)
+            else
+                for j = 1:n
+                    @inbounds unsafe_store!(pointer(out, i * n + j), codeunit(s, j))
+                end
+            end
+        end
+    end
+    return out
+end
+
+# copy/pasted from strings/util.jl
+function Base.startswith(a::PosLenString, b::Union{String, SubString{String}, PosLenString})
+    cub = ncodeunits(b)
+    if ncodeunits(a) < cub
+        false
+    elseif !escaped(a) && Base._memcmp(a, b, sizeof(b)) == 0
+        nextind(a, cub) == cub + 1
+    elseif escaped(a)
+        for i = 1:cub
+            codeunit(a, i) == codeunit(b, i) || return false
+        end
+        return true
+    else
+        false
+    end
+end
+
+function Base.endswith(a::PosLenString, b::Union{String, SubString{String}, PosLenString})
+    cub = ncodeunits(b)
+    astart = ncodeunits(a) - ncodeunits(b) + 1
+    if astart < 1
+        false
+    elseif !escaped(a) && GC.@preserve(a, Base._memcmp(pointer(a, astart), b, sizeof(b))) == 0
+        thisind(a, astart) == astart
+    elseif escaped(a)
+        for (j, i) = enumerate(astart:cub)
+            codeunit(a, i) == codeunit(b, j) || return false
+        end
+        return true
+    else
+        false
+    end
+end
+
+Base.match(r::Regex, s::PosLenString, i::Integer) = match(r, String(s), i)
+
+# the rest of these methods are copy/pasted from Base strings/string.jl file
+# for efficiency
+Base.@propagate_inbounds function Base.isvalid(x::PosLenString, i::Int)
+    @boundscheck checkbounds(Bool, x, i) || throw(BoundsError(x, i))
+    return @inbounds thisind(x, i) == i
+end
+
+Base.@propagate_inbounds function Base.thisind(s::PosLenString, i::Int)
+    i == 0 && return 0
+    n = ncodeunits(s)
+    i == n + 1 && return i
+    @boundscheck Base.between(i, 1, n) || throw(BoundsError(s, i))
+    @inbounds b = codeunit(s, i)
+    (b & 0xc0 == 0x80) & (i-1 > 0) || return i
+    @inbounds b = codeunit(s, i-1)
+    Base.between(b, 0b11000000, 0b11110111) && return i-1
+    (b & 0xc0 == 0x80) & (i-2 > 0) || return i
+    @inbounds b = codeunit(s, i-2)
+    Base.between(b, 0b11100000, 0b11110111) && return i-2
+    (b & 0xc0 == 0x80) & (i-3 > 0) || return i
+    @inbounds b = codeunit(s, i-3)
+    Base.between(b, 0b11110000, 0b11110111) && return i-3
+    return i
+end
+
+Base.@propagate_inbounds function Base.nextind(s::PosLenString, i::Int)
+    i == 0 && return 1
+    n = ncodeunits(s)
+    @boundscheck Base.between(i, 1, n) || throw(BoundsError(s, i))
+    @inbounds l = codeunit(s, i)
+    (l < 0x80) | (0xf8 ≤ l) && return i+1
+    if l < 0xc0
+        i′ = @inbounds thisind(s, i)
+        return i′ < i ? @inbounds(nextind(s, i′)) : i+1
+    end
+    # first continuation byte
+    (i += 1) > n && return i
+    @inbounds b = codeunit(s, i)
+    b & 0xc0 ≠ 0x80 && return i
+    ((i += 1) > n) | (l < 0xe0) && return i
+    # second continuation byte
+    @inbounds b = codeunit(s, i)
+    b & 0xc0 ≠ 0x80 && return i
+    ((i += 1) > n) | (l < 0xf0) && return i
+    # third continuation byte
+    @inbounds b = codeunit(s, i)
+    ifelse(b & 0xc0 ≠ 0x80, i, i+1)
+end
+
+Base.@propagate_inbounds function Base.iterate(s::PosLenString, i::Int=firstindex(s))
+    (i % UInt) - 1 < ncodeunits(s) || return nothing
+    b = @inbounds codeunit(s, i)
+    u = UInt32(b) << 24
+    Base.between(b, 0x80, 0xf7) || return reinterpret(Char, u), i+1
+    return iterate_continued(s, i, u)
+end
+
+function iterate_continued(s::PosLenString, i::Int, u::UInt32)
+    u < 0xc0000000 && (i += 1; @goto ret)
+    n = ncodeunits(s)
+    # first continuation byte
+    (i += 1) > n && @goto ret
+    @inbounds b = codeunit(s, i)
+    b & 0xc0 == 0x80 || @goto ret
+    u |= UInt32(b) << 16
+    # second continuation byte
+    ((i += 1) > n) | (u < 0xe0000000) && @goto ret
+    @inbounds b = codeunit(s, i)
+    b & 0xc0 == 0x80 || @goto ret
+    u |= UInt32(b) << 8
+    # third continuation byte
+    ((i += 1) > n) | (u < 0xf0000000) && @goto ret
+    @inbounds b = codeunit(s, i)
+    b & 0xc0 == 0x80 || @goto ret
+    u |= UInt32(b); i += 1
+@label ret
+    return reinterpret(Char, u), i
+end
+
+Base.@propagate_inbounds function Base.getindex(s::PosLenString, i::Int)
+    b = codeunit(s, i)
+    u = UInt32(b) << 24
+    Base.between(b, 0x80, 0xf7) || return reinterpret(Char, u)
+    return getindex_continued(s, i, u)
+end
+
+function getindex_continued(s::PosLenString, i::Int, u::UInt32)
+    if u < 0xc0000000
+        # called from `getindex` which checks bounds
+        @inbounds isvalid(s, i) && @goto ret
+        Base.string_index_err(s, i)
+    end
+    n = ncodeunits(s)
+
+    (i += 1) > n && @goto ret
+    @inbounds b = codeunit(s, i) # cont byte 1
+    b & 0xc0 == 0x80 || @goto ret
+    u |= UInt32(b) << 16
+
+    ((i += 1) > n) | (u < 0xe0000000) && @goto ret
+    @inbounds b = codeunit(s, i) # cont byte 2
+    b & 0xc0 == 0x80 || @goto ret
+    u |= UInt32(b) << 8
+
+    ((i += 1) > n) | (u < 0xf0000000) && @goto ret
+    @inbounds b = codeunit(s, i) # cont byte 3
+    b & 0xc0 == 0x80 || @goto ret
+    u |= UInt32(b)
+@label ret
+    return reinterpret(Char, u)
+end
+
+Base.length(s::PosLenString) = length_continued(s, 1, ncodeunits(s), ncodeunits(s))
+
+Base.@propagate_inbounds function Base.length(s::PosLenString, i::Int, j::Int)
+    @boundscheck begin
+        0 < i ≤ ncodeunits(s)+1 || throw(BoundsError(s, i))
+        0 ≤ j < ncodeunits(s)+1 || throw(BoundsError(s, j))
+    end
+    j < i && return 0
+    @inbounds i, k = thisind(s, i), i
+    c = j - i + (i == k)
+    length_continued(s, i, j, c)
+end
+
+@inline function length_continued(s::PosLenString, i::Int, n::Int, c::Int)
+    i < n || return c
+    @inbounds b = codeunit(s, i)
+    @inbounds while true
+        while true
+            (i += 1) ≤ n || return c
+            0xc0 ≤ b ≤ 0xf7 && break
+            b = codeunit(s, i)
+        end
+        l = b
+        b = codeunit(s, i) # cont byte 1
+        c -= (x = b & 0xc0 == 0x80)
+        x & (l ≥ 0xe0) || continue
+
+        (i += 1) ≤ n || return c
+        b = codeunit(s, i) # cont byte 2
+        c -= (x = b & 0xc0 == 0x80)
+        x & (l ≥ 0xf0) || continue
+
+        (i += 1) ≤ n || return c
+        b = codeunit(s, i) # cont byte 3
+        c -= (b & 0xc0 == 0x80)
+    end
+end
+
+# custom array type that returns PosLenStrings on index
+# copy:
+  # calcs total len of all strings in poslens
+  # allocates fresh data byte buffer for total len
+  # loops through and copies each strings data into new buffer, recalcs poslen
+"""
+    PosLenStringVector{T}(data, poslens, e=UInt8('\\'))
+
+Custom array 
+"""
+struct PosLenStringVector{T} <: AbstractVector{T}
+    data::Vector{UInt8}
+    poslens::Vector{PosLen}
+    e::UInt8
+end
+
+# convenience constructor for testing
+function strs(x::Vector, e=nothing)
+    len = map(x -> sizeof(coalesce(x, "")), x)
+    data = Vector{UInt8}(undef, len)
+    poslens = Vector{PosLen}(undef, length(x))
+    pos = 1
+    anymissing = false
+    for (i, s) in enumerate(x)
+        if s !== missing
+            slen = sizeof(s)
+            bytes = codeunits(s)
+            copyto!(data, pos, bytes, 1, slen)
+            poslens[i] = PosLen(pos, slen, false, e !== nothing && e in bytes)
+            pos += slen
+        else
+            anymissing = true
+            poslens[i] = PosLen(pos, 0, true, false)
+        end
+    end
+    return PosLenStringVector{anymissing ? Union{Missing, PosLenString} : PosLenString}(data, poslens, something(e, 0x00))
+end
+
+Base.IndexStyle(::Type{PosLenStringVector}) = Base.IndexLinear()
+Base.size(x::PosLenStringVector) = (length(x.poslens),)
+
+Base.@propagate_inbounds function Base.getindex(x::PosLenStringVector{T}, i::Int) where {T}
+    @boundscheck checkbounds(x, i)
+    @inbounds poslen = x.poslens[i]
+    T === Union{Missing, PosLenString} && missingvalue(poslen) && return missing
+    return PosLenString(x.data, poslen, x.e)
+end
+
+Base.isassigned(x::PosLenStringVector, i::Int) = true
+
+Base.@propagate_inbounds function Base.getindex(x::PosLenStringVector, inds)
+    A = PosLenStringVector(x.data, x.poslens[inds], x.e)
+    return A
+end
+
+Base.@propagate_inbounds function Base.setindex!(x::PosLenStringVector, val::AbstractString, i::Int)
+
+end
---
+++ test/poslenstrings.jl
@@ -0,0 +1,72 @@
+using Test, WeakRefStrings
+
+@testset "PosLenStrings" begin
+
+# PosLen
+poslen = PosLen(0, 0, true, false)
+@test WeakRefStrings.missingvalue(poslen)
+
+poslen = PosLen(0, 0, false, true)
+@test WeakRefStrings.escapedvalue(poslen)
+
+poslen = PosLen(1, 3, false, false)
+@test WeakRefStrings.getpos(poslen) == 1
+@test WeakRefStrings.getlen(poslen) == 3
+
+@test_throws ArgumentError PosLen(1, 1048576, false, false)
+
+# PosLenString
+for y in ("", "hey", "🍕", "\\\\hey\\\\hey")
+    x = WeakRefStrings.str(y, '\\')
+    y = unescape_string(y)
+    @test codeunits(x) == codeunits(y)
+    @test sizeof(x) == sizeof(y)
+    @test ncodeunits(x) == ncodeunits(y)
+    @test length(x) == length(y)
+    @test codeunit(x) == UInt8
+    @test lastindex(x) == lastindex(y)
+    @test isempty(x) == isempty(y)
+    @test String(x) == y
+    @test Symbol(x) == Symbol(y)
+    @test Vector{UInt8}(x) == Vector{UInt8}(y)
+    @test Array{UInt8}(x) == Array{UInt8}(y)
+    @test isascii(x) == isascii(y)
+    @test x * x == y * y
+    @test x^5 == y^5
+    @test string(x) == string(y)
+    @test join([x, x]) == join([y, y])
+    @test reverse(x) == reverse(y)
+    y != "" && @test startswith(x, x[1]) == startswith(y, x[1])
+    y != "" && @test endswith(x, x[1]) == endswith(y, x[1])
+    y != "" && @test findfirst(==(x[1]), x) === findfirst(==(x[1]), y)
+    y != "" && @test findlast(==(x[1]), x) === findlast(==(x[1]), y)
+    y != "" && @test findnext(==(x[1]), x, 1) === findnext(==(x[1]), y, 1)
+    y != "" && @test findprev(==(x[1]), x, length(x)) === findprev(==(x[1]), y, length(x))
+    r1 = match(Regex(x), x)
+    r2 = match(Regex(y), y)
+    @test r1 === r2 || r1.match == r2.match
+    for i = 1:ncodeunits(x)
+        @test codeunit(x, i) == codeunit(y, i)
+        @test isvalid(x, i) == isvalid(y, i)
+        @test thisind(x, i) == thisind(y, i)
+        @test nextind(x, i) == nextind(y, i)
+        @test prevind(x, i) == prevind(y, i)
+        @test iterate(x, i) == iterate(y, i)
+    end
+    for i = 1:length(x)
+        if isvalid(x, i)
+            @test x[i] == y[i]
+        end
+    end
+    @test x == x
+    @test x == y
+    @test y == x
+    @test hash(x) == hash(y)
+    @test cmp(x, x) == 0
+    @test cmp(x, y) == 0
+    @test cmp(y, x) == 0
+end
+
+
+
+end
